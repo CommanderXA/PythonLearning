b201ae94a28cba1f043c77971478bf019e514814
commanderxa <art.ostrikov@gmail.com>
2021-03-03 21:53:56 +0600

    docstrings

---
+++ TipsAndTricks/docstrings.py
@@ -0,0 +1,12 @@
+def myexpo(num1, num2):
+    '''
+    This function takes one number to the power 
+    of another number and returns the result
+
+    :param num1: This is the base
+    :param num2: This is the exponent
+    :return: The result of the calculation
+    '''
+    return num1 ** num2
+
+print(myexpo(8, 2))
